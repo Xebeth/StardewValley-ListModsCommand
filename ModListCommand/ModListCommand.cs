05cc074e6afc89ebe8f4766a5d26a2e6752c5698
Xebeth <xebeth@north-edge.com>
2020-01-07 22:28:21 +0100

    Replaced the CsvHelper package with KBCsv to ensure portability

--- ModListCommand/ModListCommand.cs
+++ ModListCommand/ModListCommand.cs
@@ -1,95 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using CsvHelper;
-using StardewModdingAPI;
+﻿using System.Collections.Generic;
 using StardewModdingAPI.Toolkit;
+using System.Globalization;
+using System.Diagnostics;
+using StardewModdingAPI;
+using System.Linq;
+using System.IO;
+using KBCsv;
+using System;
+using System.Threading.Tasks;
 
 namespace ModListCommand
 {
     public class ModListCommand : Mod
     {
-        private ModToolkit _toolkit = new ModToolkit();
+        private readonly ModToolkit _toolkit = new ModToolkit();
+        private const string _commandName = "list_mods";
 
         public override void Entry(IModHelper helper)
         {
             
-            helper.ConsoleCommands.Add("list_mods", "outputs a list of installed mods", ListMods);
+            helper.ConsoleCommands.Add(_commandName, Help(), ListMods);
         }
 
-        private string GetUpdateLinks(IEnumerable<string> updateKeys, string separator = ";")
+        public string GetUpdateLinks(IEnumerable<string> updateKeys, string separator = ";")
         {
-            return string.Join(separator, updateKeys?.Select(u => _toolkit.GetUpdateUrl(u)).DefaultIfEmpty().ToArray() ?? new[] {"no update key"});
+            return string.Join(separator, updateKeys?.Select(u => _toolkit.GetUpdateUrl(u) ?? u).DefaultIfEmpty().ToArray() ?? new[] {"no update key"});
         }
 
-        private CsvRecord CreateRecord(IManifest manifest)
-        {
-            return new CsvRecord()
-            {
-                Links = GetUpdateLinks(manifest.UpdateKeys),
-                Version = manifest.Version.ToString(),
-                Description = manifest.Description,
-                Author = manifest.Author,
-                Name = manifest.Name
-            };
-        }
-
-        private void ListMods(string commandName, string[] args)
+        private async void ListMods(string commandName, string[] args)
         {
             if (args.Length == 0 || args[0] == "console")
             {
                 foreach (var mod in Helper.ModRegistry.GetAll())
                 {
-                    var links = GetUpdateLinks(mod.Manifest.UpdateKeys);
+                    const string separator = "\n    - ";
+                    var links = GetUpdateLinks(mod.Manifest.UpdateKeys, separator);
 
-                    Monitor.Log($"{mod.Manifest.Name} v{mod.Manifest.Version} by {mod.Manifest.Author} {links} :\n"
-                              + $"\t{mod.Manifest.Description}", LogLevel.Info);
+                    Monitor.Log($"{mod.Manifest.Name} v{mod.Manifest.Version} by {mod.Manifest.Author}:{separator}{links}\n"
+                              + $"{mod.Manifest.Description}", LogLevel.Info);
                 }
             }
             else if (args.Length == 2 && args[0] == "csv" && !string.IsNullOrWhiteSpace(args[1]))
-            {
-                using (var mem = new MemoryStream())
-                using (var writer = new StreamWriter(mem))
-                using (var csvWriter = new CsvWriter(writer))
+            { 
+                try
+                { 
+                    using (var streamWriter = new StreamWriter(args[1]))
+                    using (var writer = new CsvWriter(streamWriter)
+                    {
+                        ValueSeparator = CultureInfo.CurrentCulture.TextInfo.ListSeparator[0],
+                        ValueDelimiter = '"',
+                        ForceDelimit = true
+                    })
+                    {
+                        try
+                        {
+                            var tasks = Helper.ModRegistry.GetAll().Select(e => writer.WriteRecordAsync(e.Manifest.Name, e.Manifest.Version.ToString(), e.Manifest.Author,
+                                                                           GetUpdateLinks(e.Manifest.UpdateKeys), e.Manifest.Description));
+                            
+                            await writer.WriteRecordAsync("Name", "Version", "Author", "Links", "Description");
+                            await Task.WhenAll(tasks);
+                            Process.Start(args[1]);
+
+                            Monitor.Log($"{writer.RecordNumber} records written to `{args[1]}`", LogLevel.Info);
+                        }
+                        catch (Exception e)
+                        {
+                            Monitor.Log(e.Message, LogLevel.Error);
+                        }
+                    }
+                }
+                catch(Exception e)
                 {
-                    try
-                    {
-                        var data = Helper.ModRegistry.GetAll().Select(m => CreateRecord(m.Manifest));
-
-                        csvWriter.Configuration.Delimiter = CultureInfo.CurrentCulture.TextInfo.ListSeparator;
-                        csvWriter.Configuration.ShouldQuote = (s, context) => true;
-                        csvWriter.Configuration.HasHeaderRecord = true;
-                        csvWriter.Configuration.AutoMap<CsvRecord>();
-
-                        csvWriter.WriteHeader<CsvRecord>();
-                        csvWriter.NextRecord();
-                        csvWriter.WriteRecords(data);
-                        writer.Flush();
-
-                        File.WriteAllLines(args[1], new[] { Encoding.UTF8.GetString(mem.ToArray()) });
-                        Monitor.Log($"List of mods written to `${args[1]}`", LogLevel.Info);
-                        Process.Start(args[1]);
-                    }
-                    catch (Exception e)
-                    {
-                        Monitor.Log(e.Message, LogLevel.Error);
-                    }
+                    Monitor.Log(e.Message, LogLevel.Error);
                 }
             }
             else
             {
-                ShowUsage(commandName);
+                Monitor.Log($"Command usage: {commandName} {Help()}", LogLevel.Warn);
             }
         }
 
-        private void ShowUsage(string commandName)
+        private static string Help()
         {
-            Monitor.Log($"{commandName} [console|csv] [csvFile]", LogLevel.Warn);
+            return "[console|csv] [csvFile]";
         }
     }
 }
